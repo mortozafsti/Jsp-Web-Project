cd8cb4ddf5c2403d88967c90381e18928f2a3185
g m mortoza ahmed <mortozafsti@gmail.com>
2019-01-10 12:53:41 +0600

    java Update

---
+++ PrimeFaces/src/java/bean/dashboardBean.java
@@ -0,0 +1,70 @@
+package bean;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.CloseEvent;
+import org.primefaces.event.DashboardReorderEvent;
+import org.primefaces.event.ToggleEvent;
+import org.primefaces.model.DashboardColumn;
+import org.primefaces.model.DashboardModel;
+import org.primefaces.model.DefaultDashboardColumn;
+import org.primefaces.model.DefaultDashboardModel;
+
+@ManagedBean
+@ViewScoped
+public class dashboardBean implements Serializable {
+
+    private DashboardModel model;
+
+    @PostConstruct
+    public void init() {
+        model = new DefaultDashboardModel();
+        DashboardColumn column1 = new DefaultDashboardColumn();
+        DashboardColumn column2 = new DefaultDashboardColumn();
+        DashboardColumn column3 = new DefaultDashboardColumn();
+
+        column1.addWidget("calender");        
+        column1.addWidget("calculator");        
+        column1.addWidget("contacts"); 
+        
+        column2.addWidget("dictionary");  
+        column2.addWidget("mortoza");  
+        
+        column3.addWidget("weather");
+        column3.addWidget("translation");
+
+        model.addColumn(column1);
+        model.addColumn(column2);
+        model.addColumn(column3);
+
+    }
+
+    public void handleReorder(DashboardReorderEvent event) {
+        FacesMessage message = new FacesMessage();
+        message.setSeverity(FacesMessage.SEVERITY_INFO);
+        message.setSummary("Reordered: " + event.getWidgetId());
+        message.setDetail("Item index: " + event.getItemIndex() + ", Column index: " + event.getColumnIndex() + ", Sender index: " + event.getSenderColumnIndex());
+
+        addMessage(message);
+    }
+
+    
+
+    public DashboardModel getModel() {
+        return model;
+    }
+
+    public void setModel(DashboardModel model) {
+        this.model = model;
+    }
+
+    private void addMessage(FacesMessage message) {
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+
+}

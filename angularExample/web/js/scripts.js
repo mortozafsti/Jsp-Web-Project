adbf02b4eb98818bab33896aac5f406e2e127fdd
G M Mortoza Ahmed <mortozafsti@gmail.com>
2019-01-26 07:51:21 +0600

    java update

---
+++ angularExample/web/js/scripts.js
@@ -0,0 +1,19 @@
+<script src="js/angular.min.js"></script>
+
+var app = angular.module("muApp", ["ngRoute"]);
+  app.config(function ($routeProvider) {
+    $routeProvider.when("/home", {
+        templateUrl: "template/home.html",
+        controller: "homecontroller" 
+    }).when("/course", {
+        templateUrl: "template/course.html",
+        controller: "coursecontroller" 
+    });
+  });
+app.controller("homecontroller", function ($scope){ 
+    $scope.homehtml = "This is Home Page";
+});
+app.controller("coursecontroller", function ($scope){ 
+    $scope.course = ["C#","VB.Net","Java","PHP","Angular JS"];
+});
+

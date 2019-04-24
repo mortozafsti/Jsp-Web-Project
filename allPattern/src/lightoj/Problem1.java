06c4780bad1cdedda2a2888c4707fd6cc4b39c02
G M Mortoza Ahmed <mortozafsti@gmail.com>
2019-04-25 00:21:37 +0600

    java update

---
+++ allPattern/src/lightoj/Problem1.java
@@ -0,0 +1,15 @@
+package lightoj;
+
+public class Problem1 {
+
+    public static void main(String[] args) {
+        
+        int n = 2;
+        int a = 9;
+        int b = 8;
+        
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Case " + i + " : " + (a+b));
+        }
+    }
+}

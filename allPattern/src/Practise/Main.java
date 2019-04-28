8a95a74fa54f1537ca7e35a989f234582533a5f6
G M Mortoza Ahmed <mortozafsti@gmail.com>
2019-04-29 01:51:26 +0600

    java update

---
+++ allPattern/src/Practise/Main.java
@@ -0,0 +1,21 @@
+
+
+package Practise;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scansu = new Scanner(System.in);
+
+        int a = scansu.nextInt();
+        int b = scansu.nextInt();
+
+        int X = a + b;
+
+        System.out.println("X = " + X);
+
+    }
+
+}

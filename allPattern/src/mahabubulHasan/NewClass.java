7463b9e63611d025dcf1db2ba79d9af016649820
G M Mortoza Ahmed <mortozafsti@gmail.com>
2019-05-22 02:36:34 +0600

    java update

--- allPattern/src/mahabubulHasan/NewClass.java
+++ allPattern/src/mahabubulHasan/NewClass.java
@@ -1,10 +1,17 @@
-
 package mahabubulHasan;
 
 public class NewClass {
-    
+
     public static void main(String[] args) {
+
+        int num = 5;
+
         
+        
+        do {
+            
+            System.out.println(num-- + " This is not increment");
+        } while (num == 0);
     }
-    
+
 }

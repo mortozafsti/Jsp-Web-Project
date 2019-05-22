b4d38eedd5b396c4919e75d534bfec15fcb0af7b
G M Mortoza Ahmed <mortozafsti@gmail.com>
2019-05-22 19:00:38 +0600

    java update

--- allPattern/src/mahabubulHasan/NewClass.java
+++ allPattern/src/mahabubulHasan/NewClass.java
@@ -1,17 +1,30 @@
 package mahabubulHasan;
 
-public class NewClass {
+import java.util.ArrayList;
 
-    public static void main(String[] args) {
+ class NewClass {
 
-        int num = 5;
-
-        
-        
-        do {
-            
-            System.out.println(num-- + " This is not increment");
-        } while (num == 0);
+    
+    private void init(){
+        System.out.println("Initialized");
+    }
+    private void start(){
+        init();
+        System.out.println("Started");
     }
 
+}
+class TestCall{ 
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        al.add(1);
+        al.add(2);
+        al.add(3);
+        al.add(4);
+        al.add(null);
+        al.remove(2);
+        al.remove(null);
+        
+        System.out.println(al);
+    }
 }

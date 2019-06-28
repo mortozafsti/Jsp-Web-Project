6c00a0e84ff6eeb95165d4ba702f83a39270126d
G M Mortoza Ahmed <mortozafsti@gmail.com>
2019-06-28 11:56:08 +0600

    java update

---
+++ allPattern/src/ocaAndocp/NewClass.java
@@ -0,0 +1,19 @@
+package ocaAndocp;
+
+public class NewClass {
+
+    public static void main(String[] args) {
+
+        int dayOfWeak = 5;
+        switch (dayOfWeak) {
+            case 1:
+                System.out.println("ssss");
+            default:
+                System.out.println("dddddd");
+            case 4:
+                System.out.println("aaaaaaaaaa");
+                break;
+        }
+    }
+
+}
